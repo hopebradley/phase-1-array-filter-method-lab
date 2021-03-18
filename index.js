65f5034bcbfbf01a5dff6b172594fa41efb902a3
Hope Bradley <hope.e.bradley@gmail.com>
2021-03-18 11:18:01 -0400

    commit tree ef1539c5e9a83c5ea8b4ae0b97af1e21fdfa1e8a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+  const ans = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  if (ans.length < 0) { return []; }
+  return ans;
+}
+
+function fuzzyMatch(drivers, string) {
+  const ans = drivers.filter(driver => driver.slice(0, string.length) === string);
+  return ans;
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(driver => driver.name === string);
+}
